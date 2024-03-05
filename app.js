e0b14ecc31f9a1a87d1a28d4d51855fca9628a30
BadarAhmedSheikh <badar@example.com>
2024-03-06 01:35:25 +0500

    add module and export

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-// let fatherName = "BadarAhmedSheikh";
-// let fatherName1 = "BadarAhmedSheikh-1";
-// let fatherName22 = "BadarAhmedSheikh-22";
-// export default {
-//   fatherName,fatherName1
-// };
-// export  fatherName22;
 let fatherName = "BadarAhmedSheikh";
 let fatherName1 = "BadarAhmedSheikh-1";
 let fatherName22 = "BadarAhmedSheikh-22";
-export default {
+let q = {
     fatherName,
     fatherName1
 };
-export { fatherName22 };
+let u;
+let obj1 = {
+    fatherName,
+    fatherName1
+};
+export default obj1;
+let w = {
+    fatherName,
+    fatherName1
+};
+export { q, w,u };
+export { fatherName22 as h };
--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
-import abc, { h } from "./app.js";
+import abc, { q, w, h,u } from "./app.js";
 let { fatherName, fatherName1 } = abc;
 async function main() {
     let answers = await inquirer.prompt([
@@ -13,6 +13,8 @@ async function main() {
     console.log(chalk.red("answers.user_name =", answers.user_name));
     console.log(chalk.green("fatherName =", fatherName));
     console.log(chalk.bgCyan("fatherName1 =", fatherName1));
-    console.log(chalk.blue("fatherName22 =", h));
+    console.log(chalk.blue("q =", q));
+    console.log(chalk.yellow("w =", w));
+    console.log(chalk.magenta("h =",u));
 }
 main();
